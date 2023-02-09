1ca2d48c23efa75734d6950d0cd24aca5201cf44
Anton <56392640+AntonBertrand@users.noreply.github.com>
2023-02-09 18:36:17 +0000

    Implemented Cookies & Validation
    Created links are now saved in cookies so will still be available when user refreshes page. Basic user input validation added.

--- main.js
+++ main.js
@@ -3,28 +3,58 @@ const mobileNav = document.querySelector(".mobile-menu");
 const shortenURLBtn = document.querySelector(".shorten-url");
 const urlOutput = document.querySelector(".generated-links");
 
+let savedLinks = [];
 let numberOfLinks = 0;
 
+loadCookies();
+
 function openMobileNav(){
-    console.log("boom");
     mobileNav.style.display = "block";
 }
 
 function closeMobileNav() {
-    console.log("pow");
     mobileNav.style.display = "none";
 }
 
 
 async function shortenURL() {
-    console.log("Bow");
-    let input = document.querySelector(".url-input").value;
-    let response = await fetch(`https://api.shrtco.de/v2/shorten/?url=${input}`);
-    let data = await response.json();
 
-    outputURL(data.result.short_link, input)
+    let input = document.querySelector(".url-input").value;
+
+
+    if(!validateURL(input)) {
+
+        let input = document.querySelector(".url-input").value;
+
+        console.log("valid");
+        let response = await fetch(`https://api.shrtco.de/v2/shorten/?url=${input}`);
+        let data = await response.json();
+    
+        outputURL(data.result.short_link, input)
+    }         
+
 
 }
+
+function validateURL(input) {
+
+    if (!input) {
+        const urlInput = document.querySelector(".url-input");
+        urlInput.placeholder = "Enter a valid URL";
+        urlInput.style.border = "solid 2px red";
+
+        setTimeout(function () {
+            urlInput.style.border = "none";
+        }, 3000);
+
+
+        return true;
+    } else {
+        return false;
+    }
+}
+
+
 
 function outputURL(shortLink, fullLink) {
 
@@ -45,8 +75,62 @@ let generatedURL = document.createElement("generated-link");
     generatedURL.innerHTML = generatedHTML;
     urlOutput.appendChild(generatedURL);
     numberOfLinks = numberOfLinks + 1;
+
+
+    /* Save to cookies */
+
+    let savedLink = {
+        longLink: fullLink,
+        shortenedLink: shortLink,
+    }
+
+    savedLinks.push(savedLink);
+
+    document.cookie = 'savedLinks=' + JSON.stringify(savedLinks);
+
 }
 
+ function loadCookies() {
+
+    let tempSavedLinks = getCookie();
+    
+
+    if (tempSavedLinks != null) {
+        for (i = 0 ; i < tempSavedLinks.length; i++) {
+
+
+            let generatedHTML = '';
+            let generatedURL = document.createElement("generated-link");
+            
+    
+            generatedHTML += 
+            `
+            <div class="generated-link">
+                <p class="long-url">${tempSavedLinks[i].longLink}</p>
+                <p class="short-url" >${tempSavedLinks[i].shortenedLink}</p>
+                <button id="${numberOfLinks+1}" onClick="copyClipboard(this.id)" >Copy</button>
+            </div>
+            `
+    
+    
+        generatedURL.innerHTML = generatedHTML;
+        urlOutput.appendChild(generatedURL);
+        numberOfLinks = numberOfLinks + 1;
+    
+    
+        } 
+    } else {
+        console.log("No saved searches to store");
+    }
+
+    
+} 
+
+function read_cookie(name) {
+    var result = document.cookie.match(new RegExp(name + '=([^;]+)'));
+    result && (result = JSON.parse(result[1]));
+    return result;
+   }
 
 
 function copyClipboard(clickedID) {
@@ -64,3 +148,46 @@ function copyClipboard(clickedID) {
     navigator.clipboard.writeText(copyBtn.previousElementSibling.textContent);
     
 }
+
+function getCookie() {
+    
+    let cookieValue = '';
+    let cookieArray = new Array();
+    let result = new Array();
+  
+    //Get cookie
+    cookieValue = document.cookie;
+  
+    //Divide the cookie into an array and convert them to JSON
+    if(cookieValue){
+      cookieArray = cookieValue.split(';');
+        
+      cookieArray.forEach(data => {
+        data = data.split('=');
+  
+        //data[0]: Cookie name
+        //data[1]: Cookie value
+  
+        result[data[0]] = JSON.parse(data[1]);
+      });
+    }
+    return result['savedLinks'];
+  }
+
+  function clearCookies() {
+
+    console.log("Cookies Cleared");
+
+    // retrieve all cookies
+    var Cookies = document.cookie.split(';');
+    // set past expiry to all cookies
+    for (var i = 0; i < Cookies.length; i++) {
+       document.cookie = Cookies[i] + "=; expires="+ new Date(0).toUTCString();
+    }
+  }
+
+
+
+
+
+  
